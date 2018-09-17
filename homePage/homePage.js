dab504fd7bdd5a705192e5bea5e184641d524118
TaylorA <TArmstrong1327@gmail.com>
2018-09-17 14:09:52 -0600

    about to add movie list

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         let tempF = response.data.temp_f // temperature in Fahrenheit
         let visibilityMiles = response.data.vis_mi // visibility in miles
         // let visibilityKm = response.data.vis_mi // visibility in miles
-        let windDir = response.data.winddir_deg // wind direction in degrees
+        let windDir = response.data.winddir_compass // wind direction in degrees
         let windSpdKmh = response.data.windspd_kmh // wind speed kmh
         let windSpdMph = response.data.windspd_mph // wind speed mph
         let weatherDesc = response.data.wx_desc // weather description
@@ -90,62 +90,93 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // Insert data into DOM, with some DOM creation/manipulation
         let weatherList = document.querySelector('#weatherIcon p.weather-text')
         // console.log('weatherList', weatherList);
-        document.getElementById('weatherDescription').innerText = `Your location is latitude: ${latitude}, longitude: ${longitude}. It is ${tempF}° Fahrenheit, but it feels like ${flTempF}° Fahrenheit. You currently have ${visibilityMiles} miles of visibility. The wind is blowing ${windSpdMph} Mph at ${windDir}°. The current weather is ${weatherDesc}.`
-})
-    //**API
+        document.getElementById('weatherDescription').innerText = `Your location is latitude: ${latitude}, longitude: ${longitude}. It is ${tempF}° Fahrenheit, but it feels like ${flTempF}° Fahrenheit. You currently have ${visibilityMiles} miles of visibility. The wind is blowing ${windDir} at ${windSpdMph} Mph. The current weather is ${weatherDesc}.`
 
-    //**      Input Difficulty & Distance of Hike     **//
-    // let url = `http://api.weatherunlocked.com/api/current/${hikeLat},${hikeLon}?app_id=8e567820&app_key=e231783424f10444f19408b224fbbbb9`
-    // axios.get(url)
-    //   .then((response) => {
-    let url2 = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=${dist}&minLength=${min}&key=112496723-8c25d2a96a12588709652b75e8813b84`
-    axios.get(url2)
-      .then((response) => {
-        // console.log(response.status)
-        // console.log(response.data)
-        // console.log(response.data.trails.length)
-        // console.log(response.data.trails['0'].name)
-        if (response.data.trails.length === 0) {
-          document.getElementById('hikeList').innerText = 'No Hikes In This Area!'
-        }
-        //** Input Data into list
-        // loop over data, creating <li>'s inside an <ol>
-        let hikeList = document.getElementById('hikeList')
-        // create ol
-        let ul = document.createElement('ul')
-        // for (var i = 0; i < weatherData.length; i++) {
-        // create li
-        let tD = response.data.trails
+        // if (response.data.wx_desc === 'Partly cloudy') {
+        // //**      Input Difficulty & Distance of Hike     **//
+        // let url3 = ``
+        // axios.get(url3)
+        //   .then((response) => {
+        //     //let mD = response.data
+        //     console.log(response.status)
+        //     console.log(response.data)
+        //
+        //     //** Input Data into list
+        //     // loop over data, creating <li>'s inside an <ol>
+        //     // create ol
+        //     let ul = document.createElement('ul')
+        //     // create li
+        //
+        //
+        //     for (let i = 0; i < tD.length; i++) {
+        //       clearContent()
+        //       let li = document.createElement('li')
+        //       let a = document.createElement('a')
+        //       let lat = `${tD[i].longitude}`
+        //       li.innerText += `Movie name:
+        //       `
+        //       // a.innerText += `More Information
+        //       //
+        //       // `
+        //       // a.href += `${tD[i].url}`
+        //       ul.appendChild(li)
+        //       ul.appendChild(a)
+        //     }
+        //     movieList.appendChild(ul)
+        //
+        //     //** Input Data into list
+        //   })
+        // }
+        //     return document.getElementById('hikeList').innerText = 'Find A Movie'
+        //   }
 
-        //li.innerText = `${movies[i].Title} (${movies[i].Year})`
-        for (let i = 0; i < tD.length; i++) {
-          clearContent()
-          let li = document.createElement('li')
-          let a = document.createElement('a')
-          li.innerText += `Hike name: ${tD[i].name}, Location: ${tD[i].location}
-      Summary: ${tD[i].summary}
-      Length: ${tD[i].length} miles, Difficulty: ${tD[i].difficulty}
-      Highest Elevation: ${tD[i].high}ft, Lowest Elevation: ${tD[i].low}ft
-      Ascent: ${tD[i].ascent}ft, Descent: ${tD[i].descent}ft
-      Condition Status: ${tD[i].conditionStatus} from ${tD[i].conditionDate}.
-      Voted ${tD[i].stars} stars from ${tD[i].starVotes} reviews.
-      Local Weather: ${tD[i].lat}
-      `
-          a.innerText += `More Information
+        //**API
 
-      `
-          a.href += `${tD[i].url}`
+        //**      Input Difficulty & Distance of Hike     **//
+        let url2 = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=${dist}&minLength=${min}&key=112496723-8c25d2a96a12588709652b75e8813b84`
+        axios.get(url2)
+          .then((response) => {
+            let tD = response.data.trails
+            // console.log(response.status)
+            // console.log(response.data)
+            // console.log(response.data.trails.length)
+            // console.log(response.data.trails['0'].name)
+            if (response.data.trails.length === 0) {
+              let hikeList = document.getElementById('hikeList')
+              hikeList.innerText = 'No Hikes In This Area!'
+            }
+            //** Input Data into list
+            // loop over data, creating <li>'s inside an <ol>
+            // create ol
+            let ul = document.createElement('ul')
+            // create li
 
-          // li.innerText = More Information
-          // li.href += `${tD[i].url}`
 
-          ul.appendChild(li)
-          ul.appendChild(a)
-        }
-        hikeList.appendChild(ul)
-        //** Input Data into list
+            for (let i = 0; i < tD.length; i++) {
+              clearContent()
+              let li = document.createElement('li')
+              let a = document.createElement('a')
+              let lat = `${tD[i].longitude}`
+              li.innerText += `Hike name: ${tD[i].name}, Location: ${tD[i].location}
+              Summary: ${tD[i].summary}
+              Length: ${tD[i].length} miles, Difficulty: ${tD[i].difficulty}
+              Highest Elevation: ${tD[i].high}ft, Lowest Elevation: ${tD[i].low}ft
+              Ascent: ${tD[i].ascent}ft, Descent: ${tD[i].descent}ft
+              Condition Status: ${tD[i].conditionStatus} from ${tD[i].conditionDate}.
+              Voted ${tD[i].stars} stars from ${tD[i].starVotes} reviews.
+              Local Weather: ${tD[i].longitude}
+              `
+              a.innerText += `More Information
+
+              `
+              a.href += `${tD[i].url}`
+              ul.appendChild(li)
+              ul.appendChild(a)
+            }
+            hikeList.appendChild(ul)
+            //** Input Data into list
+          })
       })
-    // })
     //**      Input Difficulty & Distance of Hike     **//
   })
 
@@ -218,41 +249,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
     return dist
   }
   /*      Render Distance if it is stored in Local Storage      */
+
+  // let url2 = 'https://unsplash.com/search/photos/rain'
+  // axios.get(url2)
+  // .then((response) => {
+  // console.log(response.status)
+  // console.log(response.data)
+  // })
+
+  // //**      Fade out Any Element you Click On     **//
+  //
+  // function fadeMeOut(item) {
+  //   let op = 1;
+  //   item.style.opacity = 1;
+  //   let fadeOut = setInterval(function() {
+  //     item.style.opacity = op;
+  //     op -= 0.02;
+  //   }, 50);
+  //   if (item.style.opacity === 0.1) {
+  //     item.style.opacity = 0;
+  //     clearInterval(fadeOut)
+  //   }
+  // }
+  //
+  // document.addEventListener('click', function(event) {
+  //   let item = event.target;
+  //   fadeMeOut(item);
+  // })
+  // //**      Fade out Any Element you Click On     **//
+
 })
-
-
-
-// let url2 = 'https://unsplash.com/search/photos/rain'
-// axios.get(url2)
-// .then((response) => {
-// console.log(response.status)
-// console.log(response.data)
-// })
-
-
-
-//**      Fade out Any Element you Click On     **//
-
-// function fadeMeOut(item) {
-//   let op = 1;
-//   item.style.opacity = 1;
-//   let fadeOut = setInterval(function() {
-//     item.style.opacity = op;
-//     op -= 0.02;
-//   }, 50);
-//   if (item.style.opacity === 0.1) {
-//     item.style.opacity = 0;
-//     clearInterval(fadeOut)
-//   }
-// }
-//
-// document.addEventListener('click', function(event) {
-//   let item = event.target;
-//   fadeMeOut(item);
-// })
-//**      Fade out Any Element you Click On     **//
-
-
 
 //** Input Data into list
 // // loop over data, creating <li>'s inside an <ol>
