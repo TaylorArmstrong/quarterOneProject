8f90a56db3f728247b4d08a122181bf2f3b8ae23
TaylorA <TArmstrong1327@gmail.com>
2018-09-18 00:45:58 -0600

    Added alternative activities when raining, need to fix new search field to work

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -23,13 +23,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
   renderMin()
   renderDist()
 
-  var formInput = document.getElementById('myForm')
+  var form = document.getElementById('myForm')
 
-  formInput.addEventListener('focus', function(event) {
+  form.addEventListener('focus', function(event) {
     event.target.style.background = 'pink'
   }, true)
 
-  formInput.addEventListener('blur', function(event) {
+  form.addEventListener('blur', function(event) {
     event.target.style.background = ''
     //**      input convert city to lat & lon (latitude & longitude search parameters)      **//
     let loc = document.getElementById('loc').value
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //**      input convert city to lat & lon (latitude & longitude search parameters)      **//
   }, true)
 
-  formInput.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function(event) {
     event.preventDefault()
 
     //**      input data to local storage on Submit     **//
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // console.log('weatherList', weatherList);
         document.getElementById('weatherDescription').innerText = `Your location is latitude: ${latitude}, longitude: ${longitude}. It is ${tempF}° Fahrenheit, but it feels like ${flTempF}° Fahrenheit. You currently have ${visibilityMiles} miles of visibility. The wind is blowing ${windDir} at ${windSpdMph} Mph. The current weather is ${weatherDesc}.`
         console.log(response.data.wx_desc)
-        if ((response.data.wx_desc === 'Partly cloudy') || (response.data.wx_desc === 'Sunny skies') || (response.data.wx_desc === 'Clear skies') || (response.data.wx_desc === 'Cloudy') || (response.data.wx_desc === 'Overcast skies') || (response.data.wx_desc === 'Mostly cloudy')) {
+        if ((response.data.wx_desc === 'Partly cloudy') || (response.data.wx_desc === 'Sunny skies') || (response.data.wx_desc !== 'Clear skies') || (response.data.wx_desc === 'Cloudy') || (response.data.wx_desc === 'Overcast skies') || (response.data.wx_desc === 'Mostly cloudy')) {
           //**      Input Difficulty & Distance of Hike     **//
           let url2 = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=${dist}&minLength=${min}&key=112496723-8c25d2a96a12588709652b75e8813b84`
           axios.get(url2)
@@ -139,44 +139,71 @@ document.addEventListener('DOMContentLoaded', (event) => {
               hikeList.appendChild(ul)
               //** Input Data into list
             }) } else {
-            //**      Input Difficulty & Distance of Hike     **//
+            // let movies = document.querySelector('div .movies')
+            let movies = document.getElementById('movieList')
+            let input = document.createElement('INPUT')
+            let submitMovie = document.createElement('button')
+            input.setAttribute('type', 'text')
+            input.setAttribute('class', 'form-control')
+            input.setAttribute('id', 'movieSearch')
+            submitMovie.setAttribute('type', 'submit')
+            submitMovie.innerText = 'Find an Event Near You'
+            movies.appendChild(input)
+            movies.appendChild(submitMovie)
+            // return document.getElementById('hikeList').innerText = 'Find A Movie'
+            let form1 = document.getElementById('myForm1')
+
+            // form1.addEventListener('submit', function(event) {
+            //   // event.preventDefault()
+            //
+            //   //**      input data to local storage on Submit     **//
+            //   let inputMovie = document.getElementById('movieSearch').value
+            //   setMovie(inputMovie.value)
+            //   renderMovie()
+              let movie = localStorage.getItem('movie')
+
 
 
         //**      Input Difficulty & Distance of Hike     **//
-        // let url3 = ``
-        // axios.get(url3)
-        //   .then((response) => {
-        //     //let mD = response.data
-        //     console.log(response.status)
-        //     console.log(response.data)
-        //
-        //     //** Input Data into list
-        //     // loop over data, creating <li>'s inside an <ol>
-        //     // create ol
-        //     let ul = document.createElement('ul')
-        //     // create li
-        //
-        //
-        //     for (let i = 0; i < tD.length; i++) {
-        //       clearContent()
-        //       let li = document.createElement('li')
-        //       let a = document.createElement('a')
-        //       let lat = `${tD[i].longitude}`
-        //       li.innerText += `Movie name:
-        //       `
-        //       // a.innerText += `More Information
-        //       //
-        //       // `
-        //       // a.href += `${tD[i].url}`
-        //       ul.appendChild(li)
-        //       ul.appendChild(a)
-        //     }
-        //     movieList.appendChild(ul)
-        //
-        //     //** Input Data into list
-        //   })
-            return document.getElementById('hikeList').innerText = 'Find A Movie'
-          }
+        let url3 = `https://app.ticketmaster.com/discovery/v2/events.json?city=Boulder&apikey=XW7jDcSV45kxGpDV6GQRxxGvrmGssyz4`
+        //let url3 = `https://app.ticketmaster.com/discovery/v2/events.json?city=${movie}&apikey=XW7jDcSV45kxGpDV6GQRxxGvrmGssyz4`
+        axios.get(url3)
+          .then((response) => {
+            console.log(response.status)
+            console.log(response.data._embedded.events[0])
+            let movies = document.getElementById('movieList')
+            //** Input Data into list
+            // loop over data, creating <li>'s inside an <ol>
+            // create ol
+            let ul = document.createElement('ul')
+            // create li
+
+            for (let i = 0; i < 10; i++) {
+              clearContent()
+              let eList = response.data._embedded.events
+              let li = document.createElement('li')
+              let a = document.createElement('a')
+              let img = document.createElement('img')
+              img.setAttribute('width', '20%')
+              li.innerText += `Event: ${eList[i].name}
+              Date: ${eList[i].dates.start.dateTime}
+              `
+              a.innerText += `More Information
+
+              `
+              a.href += `${eList[i].url}`
+              img.src += `${eList[i].images[1].url}`
+              ul.appendChild(li)
+              ul.appendChild(a)
+              ul.appendChild(img)
+            }
+            movies.appendChild(ul)
+
+            //** Input Data into list
+          })
+
+
+        }
       })
         //**      API - fetch location data from weatherUnlocked.     **//
   })
@@ -215,6 +242,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
   /*     Set Distance in Local Storage function     */
 
+  /*     Set Event Search City in Local Storage function     */
+  function setMovie(movie) {
+    let inputMovie = document.getElementById('movieSearch')
+    localStorage.setItem('movie', movie)
+  }
+  /*     Set Event Search City in Local Storage function     */
+
   /*      Render latitude if it is stored in Local Storage      */
   function renderLat() {
     let lat = 0
@@ -248,6 +282,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const localDist = localStorage.getItem('dist')
     dist = localDist
     return dist
+  }
+  /*      Render Distance if it is stored in Local Storage      */
+
+  /*      Render Distance if it is stored in Local Storage      */
+  function renderMovie() {
+    let movie = ''
+    const localMovie = localStorage.getItem('movie')
+    movie = localMovie
+    return movie
   }
   /*      Render Distance if it is stored in Local Storage      */
 
